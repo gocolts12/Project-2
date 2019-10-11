a194faa926a509d250d140263916d79ee119e7e2
mshawa2 <mshawa2@uic.edu>
2019-10-11 09:44:24 -0500

    got triggers working maybe

---
+++ Project2-master/Assets/Lysol_can_script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lysol_can_script : MonoBehaviour
+{
+    public AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider box)
+    {
+        audio.Play();
+    }
+}
---
+++ Project2-master/Assets/pickle_rick_script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickle_rick_script : MonoBehaviour
+{
+    public AudioSource audio1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider box)
+    {
+        audio1.Play();
+    }
+}
