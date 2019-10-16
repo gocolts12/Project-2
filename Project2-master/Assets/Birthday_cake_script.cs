93470ae0d50d4909081b0b7d89ea9e90077118e5
mshawa2 <mshawa2@uic.edu>
2019-10-16 14:45:06 -0500

    Final Draft 1

---
+++ Project2-master/Assets/Birthday_cake_script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birthday_cake_script : MonoBehaviour
+{
+    public AudioSource audio2;
+
+    void OnTriggerEnter(Collider box)
+    {
+        audio2.Play();
+    }
+    void OnTriggerExit(Collider other)
+    {
+        audio2.Stop();
+    }
+}
